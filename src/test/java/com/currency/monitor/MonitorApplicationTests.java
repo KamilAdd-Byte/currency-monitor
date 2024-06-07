fb5c7fab8d496346e56438aec93be6a2b9cab9bc
Kamil Sulejewski <sulejewskikamil@gmail.com>
2024-06-07 08:42:34 +0200

    feat: Initial project dependence and create docker-compose.yml. Add .gitignore.

---
+++ src/test/java/com/currency/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currency.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
