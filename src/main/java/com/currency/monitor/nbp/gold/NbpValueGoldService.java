21df24b23005f7cbe991b4f50ab4b1e3efb5e7fd
Kamil Sulejewski <sulejewskikamil@gmail.com>
2024-06-08 01:53:31 +0200

    feat(csv): First version csv download file with gold result.

--- src/main/java/com/currency/monitor/nbp/gold/NbpValueGoldService.java
+++ src/main/java/com/currency/monitor/nbp/gold/NbpValueGoldService.java
@@ -1,10 +1,13 @@
 package com.currency.monitor.nbp.gold;
 
+import com.currency.monitor.export.lineitem.CSVExportProcessor;
 import com.currency.monitor.nbp.gold.dto.GoldDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -20,8 +23,12 @@ public class NbpValueGoldService {
         this.goldApiUrl = goldApiUrl;
     }
 
-    public List<GoldDto> getTenCurrencyBy() {
+    public List<GoldDto> getTenGoldResultValue() throws IOException {
         GoldDto[] goldResult = restTemplate.getForObject(goldApiUrl, GoldDto[].class);
         return goldResult != null ? Arrays.asList(goldResult) : Collections.emptyList();
+    }
+
+    public GoldDto[] getTeenGoldResult() {
+        return restTemplate.getForObject(goldApiUrl, GoldDto[].class);
     }
 }
